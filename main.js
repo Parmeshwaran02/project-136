193b870379c488ec35c8ec807fba27f5fc7e1828
Parmeshwaran02 <81427839+Parmeshwaran02@users.noreply.github.com>
2022-01-03 19:49:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 status="";
-video="";
+objects=[];
 
-function preload(){
-    video=createVideo('video.mp4');
-    video.hide();
-
-}
 function setup(){
-    canvas=createCanvas(480,380);
+    canvas=createCanvas(380,380);
     canvas.center();
-
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="status: detecting objects";
+    document.getElementById("status").innerHTML="status: ai objects finder";
+    object_name=document.getElementById("object_name").valu ;
+
 }
 function modelLoaded(){
     console.log("model Loaded");
     status=true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+    
     
 }
 function draw(){
-image (video,0,0,480,380);
+image (video,0,0,380,380);
 }
+
